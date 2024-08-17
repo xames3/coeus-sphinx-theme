2634aff200d17f3dde95c248a63a6486e5f2c1d3
Akshay "XA" Mestry <xa@mes3.dev>
2024-08-16 21:07:44 -0500

    feat(sphinx_design & sphinx_tags): support extensions
    Signed-off-by: Akshay "XA" Mestry <xa@mes3.dev>

--- coeus_sphinx_theme/__init__.py
+++ coeus_sphinx_theme/__init__.py
@@ -4,7 +4,7 @@ Coeus Sphinx Theme
 
 Author: Akshay "XA" Mestry <xa@mes3.dev>
 Created on: Sunday, August 11 2024
-Last updated on: Thursday, August 15 2024
+Last updated on: Friday, August 16 2024
 
 This module defines the Coeus Sphinx Theme, providing utilities and
 configuration for integrating a custom theme into Sphinx documentation.
@@ -31,7 +31,7 @@ from coeus_sphinx_theme.extensions import top_ribbon
 logger = logging.getLogger(__name__)
 
 theme_name: t.Final[str] = "coeus_sphinx_theme"
-theme_version: str = "1.0.0alpha"
+theme_version: str = "1.0.0"
 
 modules: t.Sequence[types.ModuleType] = (
     contributor_hero,
@@ -39,6 +39,8 @@ modules: t.Sequence[types.ModuleType] = (
     homelander,
     top_ribbon,
 )
+
+allowed_extensions: t.Sequence[str] = ("sphinx_design", "sphinx_tags")
 
 
 def setup(app: Sphinx) -> dict[str, t.Any]:
@@ -53,6 +55,8 @@ def setup(app: Sphinx) -> dict[str, t.Any]:
         reading and writing.
     """
     here = os.path.abspath(os.path.dirname(__file__))
+    for extension in allowed_extensions:
+        app.setup_extension(extension)
     config = app.config
     coeus_theme_configurations: dict[str, tuple[t.Any, ...]] = {
         "html_coeus_author": (config.author, tuple),
@@ -69,11 +73,25 @@ def setup(app: Sphinx) -> dict[str, t.Any]:
         "html_coeus_title": (config.html_title or config.project, tuple),
         "html_coeus_twitter": ("#", str),
         "html_coeus_version": (config.release, str),
+        "html_coeus_version": (config.release, str),
+        "html_coeus_tags_create_tags": (True, bool),
+        "html_coeus_tags_create_badges": (True, bool),
+        "html_coeus_tags_prefix": ("", str),
+        "html_coeus_tags_page_title": ("Tagged", str),
     }
     for name, default in coeus_theme_configurations.items():
         app.add_config_value(name, default[0], "html", default[1])
-    config.html_permalinks_icon = config.html_coeus_permalinks_icon
+    coeus_theme_defaults: dict[str, str] = {
+        "html_permalinks_icon": "html_coeus_permalinks_icon",
+        "tags_create_tags": "html_coeus_tags_create_tags",
+        "tags_create_badges": "html_coeus_tags_create_badges",
+        "tags_intro_text": "html_coeus_tags_prefix",
+        "tags_page_title": "html_coeus_tags_page_title",
+    }
+    for default, new in coeus_theme_defaults.items():
+        setattr(config, default, getattr(config, new))
     app.add_html_theme(name=theme_name, theme_path=here)
+    app.add_css_file("theme.css", priority=900)
     app.add_js_file("theme.js", loading_method="defer")
     for module in modules:
         if module.add_html_context:
